04170776542544e164920b1bf708bc66bf2a7eb3
Aleksey Kalinovski <aleksey.kalinovski@gmail.com>
2024-08-10 03:53:26 -0400

    feat(day_2): complete part 2

--- day-02/src/part2.rs
+++ day-02/src/part2.rs
@@ -1,7 +1,38 @@
+use crate::part1::{parse_games, Color};
+
+use anyhow::anyhow;
 use anyhow::Result;
 
 pub fn process(input: &str) -> Result<i32> {
-    todo!()
+    let games;
+    match parse_games(input) {
+        Ok((_, parsed_games)) => games = parsed_games,
+        Err(_) => return Err(anyhow!("failed to parse games")),
+    }
+    let mut sum = 0;
+    for game in games {
+        let mut min_red_cube_count = 0;
+        let mut min_green_cube_count = 0;
+        let mut min_blue_cube_count = 0;
+        for round in game.rounds {
+            let mut red_cube_count = 0;
+            let mut green_cube_count = 0;
+            let mut blue_cube_count = 0;
+            for played_cube in &round.played_cubes {
+                match played_cube.1 {
+                    Color::Red => red_cube_count += played_cube.0,
+                    Color::Green => green_cube_count += played_cube.0,
+                    Color::Blue => blue_cube_count += played_cube.0,
+                };
+            }
+            min_red_cube_count = min_red_cube_count.max(red_cube_count);
+            min_green_cube_count = min_green_cube_count.max(green_cube_count);
+            min_blue_cube_count = min_blue_cube_count.max(blue_cube_count);
+        }
+        let power = min_red_cube_count * min_green_cube_count * min_blue_cube_count;
+        sum += power;
+    }
+    Ok(sum as i32)
 }
 
 #[cfg(test)]
@@ -10,9 +41,8 @@ mod tests {
 
     #[test]
     fn test_process() -> Result<()> {
-        todo!("haven't built test yet");
         let input = include_str!("../input2.txt");
-        assert_eq!(0, process(input)?);
+        assert_eq!(2286, process(input)?);
         Ok(())
     }
 }
